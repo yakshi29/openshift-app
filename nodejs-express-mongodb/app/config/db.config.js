aae27a3a36987caf13e3f5b0ab349feb89a0619d
Yakshi Behki <ybehki@us.ibm.com>
2020-09-30 19:52:25 +0530

    url change

--- nodejs-express-mongodb/app/config/db.config.js
+++ nodejs-express-mongodb/app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/mongo-test"
+    //url: "mongodb://localhost:27017/mongo-test"
+    url:"mongodb://dbuser:dbpass@localhost:27017/node-db"
   };
